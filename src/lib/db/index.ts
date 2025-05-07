7b9764fb3d596c3936f381feaecae66efb5bf3fc
ASHOK GUNJI <ashok.gunji@tekworks.in>
2025-05-07 20:39:16 +0530

    Update index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,6 +4,6 @@ import { neon } from "@neondatabase/serverless";
 
 import * as schema from "./schema";
 
-
+// Directly calling to local env file
 const sql =neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, {schema});
